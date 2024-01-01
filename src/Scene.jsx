d8f71132c714b10f941c64bbd1bf5512e7fde016
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2024-01-02 00:41:10 +0530

    first commit

---
+++ src/Scene.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import {
+  Environment,
+  OrbitControls,
+  PerspectiveCamera,
+} from "@react-three/drei";
+import {
+  ZapparCamera,
+  InstantTracker,
+  ZapparCanvas,
+} from "@zappar/zappar-react-three-fiber";
+import { Physics } from "@react-three/cannon";
+import { Suspense } from "react";
+import { Car } from "./Car";
+import { Ground } from "./Ground";
+import { Track } from "./Track";
+
+export function Scene() {
+  let [placementMode, setPlacementMode] = useState(true);
+  return (
+    <Suspense fallback={null}>
+      <ZapparCanvas>
+        <Physics broadphase="SAP" gravity={[0, -2.6, 0]}>
+          <ZapparCamera />
+          <InstantTracker
+            placementMode={true}
+            placementCameraOffset={[0, 0, -5]}
+          >
+            {/* Your 3D content */}
+            <Environment
+              files={process.env.PUBLIC_URL + "/textures/envmap.hdr"}
+              background={"both"}
+            />
+            <Track />
+            <Ground />
+            <Car />
+          </InstantTracker>
+          {/* <OrbitControls target={[-2.64, -0.71, 0.03]} /> */}
+          {/* <PerspectiveCamera makeDefault position={[-6, 3.9, 6.21]} fov={40} /> */}
+        </Physics>
+      </ZapparCanvas>
+      <div
+        id="zappar-placement-ui"
+        onClick={() => {
+          setPlacementMode((currentPlacementMode) => !currentPlacementMode);
+        }}
+        onKeyDown={() => {
+          setPlacementMode((currentPlacementMode) => !currentPlacementMode);
+        }}
+        role="button"
+        tabIndex={0}
+      >
+        Tap here to
+        {placementMode ? " place " : " pick up "}
+        the object
+      </div>
+    </Suspense>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./index.css";
+import { createRoot } from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
+import { Scene } from "./Scene";
+
+createRoot(document.getElementById("root")).render(<Scene />);
